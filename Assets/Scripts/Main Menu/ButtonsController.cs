3c71d2ed34428b0f327a25e8d0602ec5c8690aba
huseyin <huseyinsarsilmaz@hotmail.com>
2022-03-24 23:36:14 +0300

    Added Game scene and idle tower sprite

---
+++ Assets/Scripts/Main Menu/ButtonsController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour{
+    
+    public void newGameButton() {
+        SceneManager.LoadScene(1);
+    }
+}
